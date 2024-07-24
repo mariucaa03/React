181d86b7f8d5fdab0412bdbd25742c4fcee31820
mariuca <zahariuc_mariuca@yahoo.com>
2024-07-24 20:02:05 +0300

    learn1

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          Hello World! 
+          <p>You are here today to learn React!
+          Enjoy!</p>
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/person.js
@@ -0,0 +1,11 @@
+export const name = "Jesse";
+export const age = 40;
+
+//Another way to export 
+
+// const name = "Jesse"
+// const age = 40
+
+// export { name, age }
+
+import message from "./message.js";
